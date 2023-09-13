4842ba0dc43d1bf66c7022b236637121f7ce0ba9
grzegorz-kraszewski-jamf <grzegorz.kraszewski@jamf.com>
2023-09-13 20:36:58 +0200

    Add local file rename handling

--- local_file_example.py
+++ local_file_example.py
@@ -1,18 +1,23 @@
+import re
 import yaml
 import datetime
 from otomoto_scraper.scraper import OtomotoScraper
+
+
+def replace_special(string):
+    return re.sub('[^a-zA-Z0-9_]', '', string)
 
 
 if __name__ == '__main__':
     setups = yaml.safe_load(open('./scraper_setups/setup.yaml'))
     today = datetime.datetime.now().isoformat()
 
-    for setup in setups[5:7]:
+    for setup in setups[5:6]:
         region = setup['region']
         base_url = f'https://www.otomoto.pl/osobowe/uzywane/od-2015/{region}'
         params = setup['params']
         scraper = OtomotoScraper(base_url, params)
         # assuming `./data/` already exists
         upload_response = scraper.fetch_all_pages_file(
-            f'data/{scraper.__class__.__name__}_{today}_{region}_{str(params)}.csv'
+            f'data/{scraper.__class__.__name__}_{replace_special(today)}_{region}_{replace_special(str(params))}.csv'
         )
